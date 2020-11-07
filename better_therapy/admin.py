3afff28fafb6287ec8f86b61a3bd84f65ba6a1ec
dragosnechita <nechitadragos@gmail.com>
2020-11-07 10:30:56 +0200

    created custom Users, the base template and the home view

--- better_therapy/admin.py
+++ better_therapy/admin.py
@@ -1,9 +1,51 @@
 from django.contrib import admin
-from better_therapy.models import NewUser, Client, Therapist, Supervisor
+from .models import NewUser, Client, Therapist, Supervisor
+from django.contrib.auth.models import Group
+
+# #Mine: removed BaseUserAdmin, as I will use views and my own admin panel
+# from .forms import UserAdminCreationForm, UserAdminChangeForm
+# from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+# #Mine: removed BaseUserAdmin, as I will use views and my own admin panel
+
+# class UserAdmin(BaseUserAdmin):
+#     # The forms to add and change user instances
+#     form = UserAdminChangeForm
+#     add_form = UserAdminCreationForm
+#
+#     # The fields to be used in displaying the User model.
+#     # These override the definitions on the base UserAdmin
+#     # that reference specific fields on auth.User.
+#     list_display = ('email', 'is_admin',)
+#     list_filter = ('is_admin',)
+#     fieldsets = (
+#         (None, {'fields': ('email', 'password')}),
+#         ('Personal info', {'fields': ()}),
+#         ('Permissions', {'fields': ('is_admin',)}),
+#     )
+#     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+#     # overrides get_fieldsets to use this attribute when creating a user.
+#     add_fieldsets = (
+#         (None, {
+#             'classes': ('wide',),
+#             'fields': ('email', 'password1', 'password2')}
+#         ),
+#     )
+#     search_fields = ('email',)
+#     ordering = ('email',)
+#     filter_horizontal = ()
+
+
+# Remove Group Model from admin. We're not using it.
+admin.site.unregister(Group)
 
 # Register your models here.
 admin.site.register(NewUser)
+#  #Mine: gave up the form so removed UserAdmin
+# admin.site.register(NewUser, UserAdmin)
 admin.site.register(Client)
 admin.site.register(Therapist)
 admin.site.register(Supervisor)
+
+admin.site.unregister(NewUser)
 
--- better_therapy/views.py
+++ better_therapy/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from .models import Client, Therapist, Supervisor
 # Create your views here.
+
+
+class HomeView(TemplateView):
+    template_name = 'base.html'
+    title = 'Home'
